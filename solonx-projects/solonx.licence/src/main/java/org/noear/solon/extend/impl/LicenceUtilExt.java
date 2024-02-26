41cf9212c7212c5326eb3bd3d4391b1d1a2dc80f
noear <noear@live.cn>
2024-02-26 13:11:49 +0800

    添加 solonx.licence 插件

--- solonx-projects/solonx.licence/src/main/java/org/noear/solon/extend/impl/LicenceUtilExt.java
+++ solonx-projects/solonx.licence/src/main/java/org/noear/solon/extend/impl/LicenceUtilExt.java
@@ -3,15 +3,15 @@ package org.noear.solon.extend.impl;
 import org.noear.solon.Solon;
 import org.noear.solon.Utils;
 import org.noear.solon.core.util.LicenceUtil;
-import org.noear.solonx.utils.RsaUtil;
+import org.noear.solonx.licence.LicenceHelper;
 
 /**
  * @author noear
  * @since 2.7
  */
 public class LicenceUtilExt extends LicenceUtil {
-    private static final String PRIVATE_KEY = "MIIBVAIBADANBgkqhkiG9w0BAQEFAASCAT4wggE6AgEAAkEAt304v5Gshy2+3Mt2suUeF5KrhjBVEM2QPDQs+L0AUAPhXzlG6r3Yx8B0LKqKS7wUfGG/EVr6kpFmhGXlkm8lJwIDAQABAkBuDzBQyAIiey/2pBwopgFsxk8YCDmeAI85bVpkQInFc+okp5ko0WFW4iwPjNunaeie69njbFV/GNBy+G8lrmpRAiEA4mquKQE+JFSZQLE8al1XvZe54ANGkhr4wBG4/9SuK00CIQDPdqy1bOoQOE+SSmLjvg9rFsw0cxJUxw7My0Ny6jkQQwIge6oO08ClA0zdo5LqT6IJ8Ti0wiCn49ctdCJ56+lq86UCIADz7ZUju+t5JrxoRQ4AuHdocmWnRaVICMReBqBHfR2bAiEAsQzTFp3LqTIWGrARAphmO0KjUSpar039BqQwP4h1DMc=";
-    private static RsaUtil RSA512 = new RsaUtil(512);
+    private static final String publicKey = "MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBALd9OL+RrIctvtzLdrLlHheSq4YwVRDNkDw0LPi9AFAD4V85Ruq92MfAdCyqiku8FHxhvxFa+pKRZoRl5ZJvJScCAwEAAQ==";
+
 
     private String description;
 
@@ -27,21 +27,21 @@ public class LicenceUtilExt extends LicenceUtil {
                 String appLicence = Solon.cfg().appLicence();
 
                 if (Utils.isNotEmpty(appLicence)) {
-                    String licenceStr = RSA512.decryptFromBase64(appLicence, PRIVATE_KEY);
+                    String licenceStr = LicenceHelper.licenceDecode(appLicence, publicKey);
                     String[] licence = licenceStr.split(",");
 
                     if (licence.length == 3) {
                         int edition = Integer.parseInt(licence[1]);
                         if (edition == 2 || edition == 3) {
-                            //e=0 Community Edition, 1 Premium Edition, 2 Ultimate Edition
+                            //e=0 Community Edition, 2 Enterprise Premium Edition, 3 Enterprise Ultimate Edition
 
                             StringBuilder buf = new StringBuilder();
                             buf.append("Licence: ");
                             buf.append("SN=").append(licence[0]).append(", ");
                             if (edition == 3) {
-                                buf.append("Edition=Enterprise Ultimate, ");
+                                buf.append("Edition=Enterprise Ultimate Edition, ");
                             } else {
-                                buf.append("Edition=Enterprise Premium, ");
+                                buf.append("Edition=Enterprise Premium Edition, ");
                             }
                             buf.append("Authorized=").append(licence[2]);
                             description = buf.toString();
